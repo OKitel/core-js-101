cc99cdd81a5db288eb7f6817523e9ab13acf2b29
OKitel <kitelolga@gmail.com>
2023-06-06 22:47:58 +0200

    feat: add solution for sort digit names by numeric order task

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -317,8 +317,21 @@ function getPositivesCount(arr) {
  *   [ 'nine','eight','nine','eight'] => [ 'eight','eight','nine','nine']
  *   [ 'one','one','one','zero' ]     => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const order = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ];
+
+  return arr.sort((a, b) => order.indexOf(a) - order.indexOf(b));
 }
 
 /**

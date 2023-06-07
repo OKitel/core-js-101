22468be55e0d4697a298f2de16ebfae66d2c42fc
OKitel <kitelolga@gmail.com>
2023-06-07 11:43:29 +0200

    feat: add solution for get interval array task

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -481,8 +481,11 @@ function getIdentityMatrix(n) {
  *     0, 100 => [ 0, 1, 2, ..., 100 ]
  *     3, 3   => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  if (start === end) {
+    return [start];
+  }
+  return Array.from({ length: end - start + 1 }, (_, index) => start + index);
 }
 
 /**

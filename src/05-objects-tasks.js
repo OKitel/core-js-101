c7b1ec6554fae54f34db52b3f3484553c9ee5209
OKitel <kitelolga@gmail.com>
2023-06-20 23:00:58 +0200

    feat: add solution for from JSON task

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -50,8 +50,14 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const obj = JSON.parse(json);
+  const newObj = Object.create(proto);
+  const keysArr = Object.keys(obj);
+  keysArr.forEach((key) => {
+    newObj[key] = obj[key];
+  });
+  return newObj;
 }
 
 /**

d4708431795952d1b1dcea70c30216f82a3f97e1
OKitel <kitelolga@gmail.com>
2023-07-01 15:12:13 +0200

    feat: add solution for logger task

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -153,8 +153,16 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    const argsString = args.map(JSON.stringify).join(',');
+    const start = `${func.name}(${argsString}) starts`;
+    const end = `${func.name}(${argsString}) ends`;
+    logFunc(start);
+    const result = func(...args);
+    logFunc(end);
+    return result;
+  };
 }
 
 /**
